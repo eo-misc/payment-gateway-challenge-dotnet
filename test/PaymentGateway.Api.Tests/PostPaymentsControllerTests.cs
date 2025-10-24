6f079808387e9cdfae321566b28f98abad280f14
eomisc <ernest.obihara.work+1@gmail.com>
2025-10-24 19:39:25 +0100

    Add POST Bank Decline behaviour tests

--- test/PaymentGateway.Api.Tests/PostPaymentsControllerTests.cs
+++ test/PaymentGateway.Api.Tests/PostPaymentsControllerTests.cs
@@ -75,4 +75,68 @@ public partial class PaymentsControllerTests : ApiTestsBase
         Assert.Equal("GBP", savedPayment.Currency);
         Assert.Equal(100, savedPayment.Amount);
     }
+    
+    [Fact]
+    public async Task Post_GivenValidPaymentAndBankDeclines_ThenReturns200Declined()
+    {
+        var request = new PostPaymentRequest
+        {
+            CardNumber = "2222405343248888",
+            ExpiryMonth = "04",
+            ExpiryYear = "2025",
+            Currency = "GBP",
+            Amount = 100,
+            Cvv = "123"
+        };
+
+        var client = CreateClient(merchantId: "merchant-123");
+
+        var response = await client.PostAsJsonAsync("/api/payments", request);
+        var paymentResponse = await response.Content.ReadFromJsonAsync<PostPaymentResponse>();
+
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        Assert.NotNull(paymentResponse);
+        Assert.Equal(PaymentStatus.Declined, paymentResponse.Status);
+        Assert.NotEqual(Guid.Empty, paymentResponse.Id);
+        Assert.Equal("8888", paymentResponse.CardNumberLastFour);
+        Assert.Equal("04", paymentResponse.ExpiryMonth);
+        Assert.Equal("2025", paymentResponse.ExpiryYear);
+        Assert.Equal("GBP", paymentResponse.Currency);
+        Assert.Equal(100, paymentResponse.Amount);
+    }
+    
+    [Fact]
+    public async Task Post_GivenValidPaymentAndBankDeclines_ThenPersistsPaymentToDatabase()
+    {
+        // Arrange
+        var merchantId = "merchant-123";
+        var request = new PostPaymentRequest
+        {
+            CardNumber = "2222405343248112",
+            ExpiryMonth = "04",
+            ExpiryYear = "2025",
+            Currency = "GBP",
+            Amount = 100,
+            Cvv = "123"
+        };
+
+        var client = CreateClient(merchantId: merchantId);
+
+        // Act
+        var response = await client.PostAsJsonAsync("/api/payments", request);
+        var paymentResponse = await response.Content.ReadFromJsonAsync<PostPaymentResponse>();
+
+        // Assert
+        Assert.NotNull(paymentResponse);
+        var savedPayment = GetPayment(merchantId, paymentResponse.Id);
+        Assert.NotNull(savedPayment);
+        Assert.Equal(paymentResponse.Id, savedPayment.Id);
+        Assert.Equal(merchantId, savedPayment.MerchantId);
+        Assert.Equal(PaymentStatus.Declined, savedPayment.Status);
+        Assert.Equal("8112", savedPayment.CardNumberLastFour);
+        Assert.Equal("04", savedPayment.ExpiryMonth);
+        Assert.Equal("2025", savedPayment.ExpiryYear);
+        Assert.Equal("GBP", savedPayment.Currency);
+        Assert.Equal(100, savedPayment.Amount);
+    }
 }
